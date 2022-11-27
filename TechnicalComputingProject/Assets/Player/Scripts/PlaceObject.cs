7e1d93e22eaaf591c5a6c9db162ebdd750ddbd23
TyW13 <c0018068@my.shu.ac.uk>
2022-11-27 02:51:51 +0000

    Fixed object placing and added object placement position indicator
    I fixed a problem with placing of objects due to rounding towards even numbers at incorrect times. I added an indicator in the shape of a cube to show the position of where the object would be placed currently on the map.

--- TechnicalComputingProject/Assets/Player/Scripts/PlaceObject.cs
+++ TechnicalComputingProject/Assets/Player/Scripts/PlaceObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -6,46 +7,83 @@ using UnityEngine.UIElements;
 
 public class PlaceObject : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject placementZone;
     [SerializeField] 
     private GameObject objectToPlace;
     [SerializeField]
     private Camera c;
     int rayDistance = 300;
 
+    private GameObject placementZoneObj;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        placementZoneObj = Instantiate(placementZone, new Vector3(100, 100, 100), Quaternion.identity);                    // Create object at ray hit x/z position with y=1 to be above ground
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))                                                                     // If left mouse button is clicked
+        DisplayPlacementZone();
+
+        if (Input.GetMouseButtonDown(0))                                                                     // If left mouse button is clicked
         {
-            SpawnObject();
+            SpawnObject(objectToPlace);                                                                      // Spawn Object to block path
         }
     }
 
-
-    // Function to get point in world space to place object using mouse position
-    void SpawnObject()
+    // Function to display location where player will place object
+    void DisplayPlacementZone()
     {
-        Ray ray = c.ScreenPointToRay(Input.mousePosition);                                                  // Ray original position/direction = from camera to mouse on screen position
-        RaycastHit hit = new RaycastHit();                                                                  // New variable to store information on raycast hit (position, object etc.)
+        Ray ray = c.ScreenPointToRay(Input.mousePosition);                                                             // Ray original position/direction = from camera to mouse on screen position
+        RaycastHit hit = new RaycastHit();                                                                             // New variable to store information on raycast hit (position, object etc.)
+
         LayerMask g0LayerMask = LayerMask.GetMask("Ground0");
         LayerMask g1LayerMask = LayerMask.GetMask("Ground1");
         LayerMask placedLayerMask = LayerMask.GetMask("PlacedObject");
+
         if (Physics.Raycast(ray, out hit, rayDistance, g0LayerMask))
         {
-            if(Physics.CheckSphere(CalculatePosition(hit), 0.1f, placedLayerMask)                           // Returns true if another collision box overlaps with 
-                && Physics.CheckSphere(CalculatePosition(hit), 0.1f, g1LayerMask))                          // checking sphere on PlacedObject or ground1 layer
-            { 
+            if (Physics.CheckSphere(CalculatePosition(hit), 0.1f, placedLayerMask)                           // Returns true if another collision box overlaps with
+                || Physics.CheckSphere(CalculatePosition(hit), 0.1f, g1LayerMask))                          // checking sphere on PlacedObject or ground1 layer
+            {
+                placementZoneObj.SetActive(false);
+            }
+            else
+            {
+                placementZoneObj.SetActive(true);
+                placementZoneObj.transform.position = CalculatePosition(hit);
+
+            }
+        }
+        else
+        {
+            placementZoneObj.SetActive(false);
+        }
+    }
+
+    // Function to get point in world space to place object using mouse position
+    void SpawnObject(GameObject objectToSpawn)
+    {
+        Ray ray = c.ScreenPointToRay(Input.mousePosition);                                                             // Ray original position/direction = from camera to mouse on screen position
+        RaycastHit hit = new RaycastHit();                                                                             // New variable to store information on raycast hit (position, object etc.)
+
+        LayerMask g0LayerMask = LayerMask.GetMask("Ground0");
+        LayerMask g1LayerMask = LayerMask.GetMask("Ground1");
+        LayerMask placedLayerMask = LayerMask.GetMask("PlacedObject");
+
+        if (Physics.Raycast(ray, out hit, rayDistance, g0LayerMask))
+        {
+            if(Physics.CheckSphere(CalculatePosition(hit), 0.1f, placedLayerMask)                           // Returns true if another collision box overlaps with
+                || Physics.CheckSphere(CalculatePosition(hit), 0.1f, g1LayerMask))                          // checking sphere on PlacedObject or ground1 layer
+            {
                 Debug.Log("Object Detected");
             }
             else 
             {
-                Instantiate(objectToPlace, CalculatePosition(hit), Quaternion.identity);        // Create object at ray hit x/z position with y=1 to be above ground
+                Instantiate(objectToSpawn, CalculatePosition(hit), Quaternion.identity);                    // Create object at ray hit x/z position with y=1 to be above ground
             }
         }
     }
@@ -53,8 +91,8 @@ public class PlaceObject : MonoBehaviour
     // Function to get position to place new object
     Vector3 CalculatePosition(RaycastHit hit)
     {
-        float newXPos = Mathf.Round(hit.point.x);                                               // Rounding hit positions to nearest whole number
-        float newZPos = Mathf.Round(hit.point.z);
+        float newXPos = (float)Math.Round(hit.point.x, MidpointRounding.AwayFromZero);                      // Rounding hit positions to nearest even whole number (using AwayFromZero)
+        float newZPos = (float)Math.Round(hit.point.z, MidpointRounding.AwayFromZero);
 
         return new Vector3(newXPos, 1.0f, newZPos);                                             // Returning new vector3 containing position to place new object
     }
