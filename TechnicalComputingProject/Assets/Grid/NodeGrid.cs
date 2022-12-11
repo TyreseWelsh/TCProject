21005b819fb569e65163f049a5ce51bc49c7a50c
TyW13 <c0018068@my.shu.ac.uk>
2022-12-11 00:23:04 +0000

    Finished pathfinding grid
    Finished 2D pathfinding grid, fixing some bugs that were present

--- TechnicalComputingProject/Assets/Grid/NodeGrid.cs
+++ TechnicalComputingProject/Assets/Grid/NodeGrid.cs
@@ -7,11 +7,12 @@ public class NodeGrid : MonoBehaviour
 {
     LayerMask environmentMask;
     LayerMask placedObjMask;
-    public Vector2 gridWSize;
+    public Vector2 gridWSize;                                                                                                       // Grid size in world
     Node [,] grid;
     public float nodeSize;
-    Vector2Int gridLSize = new Vector2Int(0, 0);
-    
+    Vector2Int gridLSize = new Vector2Int(0, 0);                                                                                    // Grid local size
+    public Transform AIChar;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,16 @@ public class NodeGrid : MonoBehaviour
         placedObjMask = LayerMask.GetMask("PlaceObject");
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))                                                                                                 // Update Grid when Key is pressed
+        {
+            CreateGrid();
+        }
+    }
+
+    // Function to create/update grid for pathfinding
     void CreateGrid()
     {
         gridLSize.x = (int)Math.Round(gridWSize.x / nodeSize);
@@ -42,13 +53,19 @@ public class NodeGrid : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Get current node AI is on using AI's world position
+    public Node NodeFromWorldPoint(Vector3 wPosition)
     {
-        if(Input.GetKeyDown(KeyCode.N))                                                                                                 // Update Grid when Key is pressed
-        {
-            CreateGrid();
-        }
+        float percentX = ((wPosition.x - transform.position.x) + gridWSize.x / 2) / gridWSize.x;                                          // Position of player in grid by getting % on x and y
+        float percentY = ((wPosition.z - transform.position.z) + gridWSize.y / 2) / gridWSize.y;                                          //
+
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.FloorToInt(Mathf.Min(gridLSize.x * percentX, gridLSize.x - 1));
+        int y = Mathf.FloorToInt(Mathf.Min(gridLSize.y * percentY, gridLSize.y - 1));
+
+        return grid[x, y];
     }
 
     private void OnDrawGizmos()
@@ -57,10 +74,18 @@ public class NodeGrid : MonoBehaviour
 
         if (grid != null)                                                                                                                // Checking if grid is valid
         {
+            Node AINode = NodeFromWorldPoint(AIChar.position);
+
             foreach (Node node in grid)
             {
                 Gizmos.color = (node.walkable) ? Color.green : Color.red;                                                               // Setting colour of cubes to show if walkable or not
+                if (AINode.nodeWPosition == node.nodeWPosition)
+                {
+                    Gizmos.DrawWireCube(node.nodeWPosition, Vector3.one * (nodeSize - 0.1f));
+                    Gizmos.color = Color.cyan;
+                }
                 Gizmos.DrawWireCube(node.nodeWPosition, Vector3.one * (nodeSize - 0.1f));                                               // Drawing the cubes with small gap inbetween
+
             }
         }
     }
